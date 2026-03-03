d712fa80d9e95b853b4adaa68b742d45b8e30986
mkivanc27-boop <mkivanc27@gmail.com>
2026-03-03 21:05:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,77 @@
-const {Client:cln,Partials,IntentsBitField,Collection,ClientOptions,SlashCommandBuilder,CommandInteraction} = require("discord.js")
+const { Client: cln, Partials, IntentsBitField, Collection } = require("discord.js");
 const fs = require("fs");
-const readline = require("readline").createInterface({input:process.stdin});
-const functions = require("./functions");
+const readline = require("readline").createInterface({ input: process.stdin });
+
 /**
  * @typedef CommandData
- * @property {SlashCommandBuilder} data
+ * @property {any} data
  * @property {String} module
- * @property {(client:Client,interaction:CommandInteraction)=>Promise<void>} execute
+ * @property {(client: Client, interaction: any) => Promise<void>} execute
  */
-class Client extends cln{
+
+class Client extends cln {
     /**
-     * @type {Collection<String,CommandData>}
+     * @type {Collection<String, CommandData>}
      */
     commands = new Collection();
-    config = require("./config.json")
-    /**
-     * @param {ClientOptions} options 
-     */
-    constructor(options){
-      super(options);
-      for(let command of fs.readdirSync(__dirname + "/commands")
-        /**
-         * @type {CommandData}
-         */
-        const cmd = require(`./commands/${command}`);
-        if(require("./config.json").modules[cmd.module]){
-          this.commands.set(cmd.data.name,cmd);
-        }
-      }
-    }
-}
-const client = new Client({intents:[IntentsBitField.Flags.Guilds,IntentsBitField.Flags.GuildMessages,IntentsBitField.Flags.MessageContent],partials:[Partials.Message]})
-for(let event of fs.readdirSync("./events")){
-    try{
-        client.on(event.split(".")[0],async(...args)=>await require(`./events/${event}`)(client,...args))
-    }catch(e){
-        console.error(e)
-    }
-}
-readline.on("line",async(line)=>{
-  try{
-    var dat=eval(line)
-    if(dat instanceof Promise)dat=await dat
-    console.log(dat)
-  }catch(e){
-    console.log(e)
-  }
 
-})
+    config = require("./config.json");
+
+    constructor(options) {
+        super(options);
+
+        // COMMANDS
+        const commandPath = __dirname + "/commands";
+
+        if (fs.existsSync(commandPath)) {
+            for (let command of fs.readdirSync(commandPath)) {
+                const cmd = require(`./commands/${command}`);
+
+                if (this.config.modules[cmd.module]) {
+                    this.commands.set(cmd.data.name, cmd);
+                }
+            }
+        }
+
+        // EVENTS
+        const eventPath = __dirname + "/events";
+
+        if (fs.existsSync(eventPath)) {
+            for (let event of fs.readdirSync(eventPath)) {
+                try {
+                    this.on(
+                        event.split(".")[0],
+                        async (...args) =>
+                            await require(`./events/${event}`)(this, ...args)
+                    );
+                } catch (e) {
+                    console.error(e);
+                }
+            }
+        }
+    }
+}
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ],
+    partials: [Partials.Message]
+});
+
+// Console test sistemi
+readline.on("line", async (line) => {
+    try {
+        let dat = eval(line);
+        if (dat instanceof Promise) dat = await dat;
+        console.log(dat);
+    } catch (e) {
+        console.log(e);
+    }
+});
+
 client.login(client.config.token);
-module.exports=Client
+
+module.exports = Client;
